7db37a0e40be524059c0d0f3c81b2996b25ac6b9
박관우 <pgw9470@naver.com>
2020-06-17 13:30:47 +0900

    asdf

--- codeforest/src/main/java/com/btcdd/codeforest/linux/CodeTreeLinux.java
+++ codeforest/src/main/java/com/btcdd/codeforest/linux/CodeTreeLinux.java
@@ -173,10 +173,8 @@ public class CodeTreeLinux {
 
 		RunJavaLinux runJavaLinux = new RunJavaLinux(fileName, packagePath, language);
 		
-		String[] split = fileName.split(".");
-		String tmp2 = fileName.replace(".java", "");
-		String tmp = split[0];
-		runJavaLinux.createFileAsSource("java -cp " + packagePath + "/" + language + "/ " + tmp2, "gwanwoo.txt");
+		String[] split = fileName.split("\\.");
+		runJavaLinux.createFileAsSource("java -cp " + packagePath + "/" + language + "/ " + split[0], "gwanwoo.txt");
 		
 		runJavaLinux.execCompile();
 		String result = runJavaLinux.execCommand();
