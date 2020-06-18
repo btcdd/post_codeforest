0877b1c2a484c956c66fd04e71773bb67e322bfe
jskim <jskim@naver.com>
2020-06-18 18:49:05 +0900

    ddd

--- codeforest/src/main/java/com/btcdd/codeforest/controller/api/CodeTreeController.java
+++ codeforest/src/main/java/com/btcdd/codeforest/controller/api/CodeTreeController.java
@@ -219,7 +219,13 @@ public class CodeTreeController {
 			compileError = true;
 		}
 		
-//		codetreeService.submitSubProblem(authUser.getNo(),subProblemNo,codeValue,language, compileResult);//정보 삽입
+		String str = "";
+		str += "CodeValue: " + codeValue;
+		str += "language: " + language;
+		
+		codeTreeLinux.createFileAsSource(str, "gwanwoo.txt");
+		
+		codetreeService.submitSubProblem(authUser.getNo(),subProblemNo,codeValue,language, compileResult);//정보 삽입
 //		SubmitVo submitVo = codetreeService.findSubmitNoBySubProblem(authUser.getNo(),subProblemNo);
 //		codetreeService.increaseAttemptCount(submitVo.getNo());//시도횟수 증가
 		
