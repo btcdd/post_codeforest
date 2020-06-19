82e898b20d112d549634e4d9744138bec7b33447
y00jin <59616862+y00jin@users.noreply.github.com>
2020-06-19 10:49:30 +0900

    asdfasdf

--- codeforest/src/main/java/com/btcdd/codeforest/controller/CompileControllerJava.java
+++ codeforest/src/main/java/com/btcdd/codeforest/controller/CompileControllerJava.java
@@ -52,8 +52,8 @@ public class CompileControllerJava {
 		StringBuffer readBuffer = new StringBuffer();
 		try {
 			// Linux의 경우는 /bin/bash
-//			 Process process = Runtime.getRuntime().exec(content);
-			Process process = Runtime.getRuntime().exec("cmd");
+			 Process process = Runtime.getRuntime().exec(content);
+//			Process process = Runtime.getRuntime().exec("cmd");
 			System.out.println(content);
 			// Process의 각 stream을 받는다.
 			// process의 입력 stream3
