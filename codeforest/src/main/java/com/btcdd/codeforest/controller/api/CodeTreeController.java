9b872216e738711127565720a68228596adb05da
jskim <jskim@naver.com>
2020-06-21 11:58:03 +0900

    aaa

--- codeforest/src/main/java/com/btcdd/codeforest/controller/api/CodeTreeController.java
+++ codeforest/src/main/java/com/btcdd/codeforest/controller/api/CodeTreeController.java
@@ -242,9 +242,9 @@ public class CodeTreeController {
 			compileError = true;
 		}
 		 
-		codetreeService.submitSubProblem(authUser.getNo(),subProblemNo,codeValue,language, compileResult);//정보 삽입
-		SubmitVo submitVo = codetreeService.findSubmitNoBySubProblem(authUser.getNo(),subProblemNo, language);
-		codetreeService.increaseAttemptCount(submitVo.getNo());//시도횟수 증가
+//		codetreeService.submitSubProblem(authUser.getNo(),subProblemNo,codeValue,language, compileResult);//정보 삽입
+//		SubmitVo submitVo = codetreeService.findSubmitNoBySubProblem(authUser.getNo(),subProblemNo, language);
+//		codetreeService.increaseAttemptCount(submitVo.getNo());//시도횟수 증가
 		
 		map.put("compileResult", compileResult);
 		map.put("compileError", compileError);
